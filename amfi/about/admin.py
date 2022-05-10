0337a9f966db3e4e166007e284772701af01453d
Ogoke Anthony <ogokeanthony187@gmail.com>
2022-05-10 11:32:31 -0700

    rectifying admins of team and about

--- amfi/about/admin.py
+++ amfi/about/admin.py
@@ -7,7 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
     list_display = ['title', 'slug', 'status', 'created', 'updated']
     list_filter = ('status', 'created', 'updated')
-    summernote_fields = ('body', 'sub_body', 'sub_body2', 'sub_body3',)
+    summernote_fields = ('body', 'sub_body',)
     prepopulated_fields = {'slug':('title',)}
     list_editable = ('status',)
     date_hierarchy = ('created')
--- amfi/team/admin.py
+++ amfi/team/admin.py
@@ -7,7 +7,6 @@ from django_summernote.admin import SummernoteModelAdmin
 class TeamAdmin(SummernoteModelAdmin):
     list_display = ['full_name', 'designation', 'status', 'created', 'updated']
     list_filter = ('status', 'created', 'updated')
-    summernote_fields = ('testimony',)
     list_editable = ('status',)
     date_hierarchy = ('created')
 
